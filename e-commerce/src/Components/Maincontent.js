81c1f9acadbb235a380c4012b0403f3b6fd2b361
Wisdom Eli <104230238+Grandlhord@users.noreply.github.com>
2023-06-30 13:44:54 +0000

    lightbox library

--- e-commerce/src/Components/Maincontent.js
+++ e-commerce/src/Components/Maincontent.js
@@ -1,5 +1,8 @@
 import React from "react";
 import product1 from '../images/image-product-1.jpg'
+import product2 from '../images/image-product-2.jpg'
+import product3 from '../images/image-product-3.jpg'
+import product4 from '../images/image-product-4.jpg'
 import thumbnail1 from '../images/image-product-1-thumbnail.jpg'
 import thumbnail2 from '../images/image-product-2-thumbnail.jpg'
 import thumbnail3 from '../images/image-product-3-thumbnail.jpg'
@@ -10,6 +13,7 @@ import addcart from '../images/icon-cart.svg'
 
 
 export default function Maincontent(){
+
     return (
         <section className="main-content">
             <div className="showcase">
